b7565208d4e0678a1cbb0937cba4092af5c9d944
A <A@ANUJ>
2023-09-30 21:53:08 -0400

    Add project files.

---
+++ Ishita 18ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishita_18ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("\n\n");
+            Console.Write("Check whether a number is positive or negative:\n");
+            Console.Write("----------------------------------------------");
+            Console.Write("\n\n");
+            Console.Write("Input an integer : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 0) 
+
+            Console.WriteLine("{0} is a positive number.\n", num);
+    else
+                Console.WriteLine("{0} is a negative number. \n", num);
+
+        }
+    }
+}
